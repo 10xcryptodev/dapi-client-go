99acc706d9e5a9cbc6de01771724a07b679eecaf
10xcryptodev <10xcryptodev@gmail.com>
2020-06-21 10:40:19 -0300

    update build

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-GO_LOCATION=$(which go)
-GOROOT=$(echo ${GO_LOCATION%/bin/go})
+export PATH=$PATH:$HOME/go/bin
+export PATH=$PATH:/usr/local/go/bin
 
 protoc --go_out=plugins=grpc:. grpc/protos/*.proto
 go build -o ./bin/dapi .
